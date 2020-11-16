2020e62b60907266bf708b6ea7e2f18684bd7aaa
bastien <bvtbastien@gmail.com>
2020-11-16 10:50:51 +0100

    Add tabs unswipable

--- app/src/main/java/com/example/snowtam/MainActivity.java
+++ app/src/main/java/com/example/snowtam/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(MainActivity.this, Accueil.class);
+                Intent intent = new Intent(MainActivity.this, Affsnowtam.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/snowtam/Nsviewpager.java
@@ -0,0 +1,41 @@
+package com.example.snowtam;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.ViewPager;
+
+public class Nsviewpager extends ViewPager {
+    private boolean enableSwipe;
+    public Nsviewpager(@NonNull Context context) {
+        super(context);
+        init();
+    }
+    public Nsviewpager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        enableSwipe = true;
+    }
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return enableSwipe && super.onInterceptTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return enableSwipe && super.onTouchEvent(event);
+
+    }
+
+    public void setEnableSwipe(boolean enableSwipe) {
+        this.enableSwipe = enableSwipe;
+    }
+
+}
