901966780e65670e76c653f421a688278a78f3f6
bastien <bvtbastien@gmail.com>
2020-11-26 11:01:34 +0100

    temp

--- app/src/main/java/com/example/snowtam/Accueil.java
+++ app/src/main/java/com/example/snowtam/Accueil.java
@@ -6,8 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class Accueil extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
