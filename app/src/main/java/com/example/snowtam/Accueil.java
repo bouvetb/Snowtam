b805d72c1beaec0e6eef1812ae9ed0067ea1d030
teddy-lemaille <teddy-lemaille@users.noreply.github.com>
2020-11-26 11:00:51 +0100

    Acceuil avec list dynamique qui fonctionne++

--- app/src/main/java/com/example/snowtam/Accueil.java
+++ app/src/main/java/com/example/snowtam/Accueil.java
@@ -47,6 +47,7 @@ public class Accueil extends AppCompatActivity {
             {
                 arrayList.add(CodeIOCA.getText().toString());
                 adapter.notifyDataSetChanged();
+                CodeIOCA.setText("");
             }
         });
 
